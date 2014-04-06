1fdb2545d2e22d998bee0ebd0dfe1461f02b61d8
ealmansi <ealmansi@gmail.com>
2014-04-05 22:43:34 -0300

    le aplico autoformat a todos los cpp

--- src/medir_performance.cpp
+++ src/medir_performance.cpp
@@ -25,14 +25,14 @@ struct Medicion
 /*    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    */
 
 void imprimir_modo_de_uso();
-vector<Medicion> tomar_mediciones(string& problema, string& funcion_generadora, int n_max);
-void escribir_datos(vector<Medicion>& mediciones, string& nombre_archivo_salida);
+vector<Medicion> tomar_mediciones(string &problema, string &funcion_generadora, int n_max);
+void escribir_datos(vector<Medicion> &mediciones, string &nombre_archivo_salida);
 
 /*    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    *    */
 
 int main(int argc, char const *argv[])
 {
-  if(argc != 5)
+  if (argc != 5)
   {
     imprimir_modo_de_uso();
     return 0;
@@ -41,11 +41,14 @@ int main(int argc, char const *argv[])
   string problema(argv[1]), funcion_generadora(argv[2]), nombre_archivo_salida(argv[4]);
   int n_max = atoi(argv[3]);
 
-  try {
+  try
+  {
     srand(time(NULL));
     vector<Medicion> mediciones = tomar_mediciones(problema, funcion_generadora, n_max);
     escribir_datos(mediciones, nombre_archivo_salida);
-  } catch(const char* error) {
+  }
+  catch (const char *error)
+  {
     cerr << error << endl;
   }
 
@@ -62,11 +65,11 @@ void imprimir_modo_de_uso()
 }
 
 template <typename E, typename S>
-vector<Medicion> tomar_mediciones(int n_max, E (*generar_instancia)(int), S (*resolver)(const E&))
+vector<Medicion> tomar_mediciones(int n_max, E (*generar_instancia)(int), S (*resolver)(const E &))
 {
   vector<Medicion> mediciones;
   timespec inicio, fin;
-  
+
   for (int n = 1; n <= n_max; ++n)
   {
     int mediana;
@@ -83,58 +86,83 @@ vector<Medicion> tomar_mediciones(int n_max, E (*generar_instancia)(int), S (*re
     mediana = mediciones_por_n[CANT_MEDICIONES_POR_N / 2];
     mediciones.push_back(Medicion(n, mediana));
   }
-  
+
   return mediciones;
 }
 
-vector<Medicion> tomar_mediciones(string& problema, string& funcion_generadora, int n_max)
+vector<Medicion> tomar_mediciones(string &problema, string &funcion_generadora, int n_max)
 {
-  if(n_max <= 0)
+  if (n_max <= 0)
     throw "El argumento n_max es inválido.";
 
-  if(problema == "problema1")
-  { 
-    if(funcion_generadora == "generar_instancia_mejor_caso") {
+  if (problema == "problema1")
+  {
+    if (funcion_generadora == "generar_instancia_mejor_caso")
+    {
       return tomar_mediciones<Problema1::Entrada, Problema1::Salida>
-        (n_max, Problema1::generar_instancia_mejor_caso, Problema1::resolver);
-    } else if(funcion_generadora == "generar_instancia_peor_caso") {
+             (n_max, Problema1::generar_instancia_mejor_caso, Problema1::resolver);
+    }
+    else if (funcion_generadora == "generar_instancia_peor_caso")
+    {
       return tomar_mediciones<Problema1::Entrada, Problema1::Salida>
-        (n_max, Problema1::generar_instancia_peor_caso, Problema1::resolver);
-    } else if(funcion_generadora == "generar_instancia_aleatoria") {
+             (n_max, Problema1::generar_instancia_peor_caso, Problema1::resolver);
+    }
+    else if (funcion_generadora == "generar_instancia_aleatoria")
+    {
       return tomar_mediciones<Problema1::Entrada, Problema1::Salida>
-        (n_max, Problema1::generar_instancia_aleatoria, Problema1::resolver);
-    } else {
+             (n_max, Problema1::generar_instancia_aleatoria, Problema1::resolver);
+    }
+    else
+    {
       throw "El argumento funcion_generadora es inválido.";
     }
-  } else if(problema == "problema2") { 
-    if(funcion_generadora == "generar_instancia_mejor_caso") {
+  }
+  else if (problema == "problema2")
+  {
+    if (funcion_generadora == "generar_instancia_mejor_caso")
+    {
       return tomar_mediciones<Problema2::Entrada, Problema2::Salida>
-        (n_max, Problema2::generar_instancia_mejor_caso, Problema2::resolver);
-    } else if(funcion_generadora == "generar_instancia_peor_caso") {
+             (n_max, Problema2::generar_instancia_mejor_caso, Problema2::resolver);
+    }
+    else if (funcion_generadora == "generar_instancia_peor_caso")
+    {
       return tomar_mediciones<Problema2::Entrada, Problema2::Salida>
-        (n_max, Problema2::generar_instancia_peor_caso, Problema2::resolver);
-    } else if(funcion_generadora == "generar_instancia_aleatoria") {
+             (n_max, Problema2::generar_instancia_peor_caso, Problema2::resolver);
+    }
+    else if (funcion_generadora == "generar_instancia_aleatoria")
+    {
       return tomar_mediciones<Problema2::Entrada, Problema2::Salida>
-        (n_max, Problema2::generar_instancia_aleatoria, Problema2::resolver);
-    } else {
+             (n_max, Problema2::generar_instancia_aleatoria, Problema2::resolver);
+    }
+    else
+    {
       throw "El argumento funcion_generadora es inválido.";
     }
-  } else if(problema == "problema3") { 
-    if(funcion_generadora == "generar_instancia_unidimensional") {
+  }
+  else if (problema == "problema3")
+  {
+    if (funcion_generadora == "generar_instancia_unidimensional")
+    {
       return tomar_mediciones<Problema3::Entrada, Problema3::Salida>
-        (n_max, Problema3::generar_instancia_unidimensional, Problema3::resolver);
-    } else if(funcion_generadora == "generar_instancia_cuadrada") {
+             (n_max, Problema3::generar_instancia_unidimensional, Problema3::resolver);
+    }
+    else if (funcion_generadora == "generar_instancia_cuadrada")
+    {
       return tomar_mediciones<Problema3::Entrada, Problema3::Salida>
-        (n_max, Problema3::generar_instancia_cuadrada, Problema3::resolver);
-    } else {
+             (n_max, Problema3::generar_instancia_cuadrada, Problema3::resolver);
+    }
+    else
+    {
       throw "El argumento funcion_generadora es inválido.";
     }
-  } else {
+  }
+  else
+  {
     throw "El argumento problema es inválido.";
   }
 }
 
-void escribir_datos(vector<Medicion>& mediciones, string& nombre_archivo_salida)
+void escribir_datos(vector<Medicion> &mediciones, string &nombre_archivo_salida)
 {
   ofstream archivo_salida;
   archivo_salida.open(nombre_archivo_salida.c_str());
